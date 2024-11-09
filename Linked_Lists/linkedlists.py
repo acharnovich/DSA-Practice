5a4fcf1a504057bc96ae32aeabb5a1cf65f39b5b
acharnovich <159467707+andrewc4cmu@users.noreply.github.com>
2024-11-09 00:31:29 -0500

    remove duplicates

--- Linked_Lists/linkedlists.py
+++ Linked_Lists/linkedlists.py
@@ -60,6 +60,7 @@ class LinkedList:
             self.head = None
             self.tail = None
         return temp
+    # method inserts at the beginning of the list
     def prepend(self, value):
         new_node = Node(value)
         if self.length == 0:
@@ -70,6 +71,7 @@ class LinkedList:
             self.head = new_node
         self.length += 1
         return True
+    # method to pop off the head of the lest
     def pop_first(self):
         if self.length == 0:
             return None
@@ -83,7 +85,7 @@ class LinkedList:
         if self.length == 0:
             self.tail == None
         return temp
-    
+    # provided a index, gets the node 
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
@@ -94,28 +96,115 @@ class LinkedList:
             # moves temp along linked list
             temp = temp.next
         return temp
+    # sets the value of a node providing a index
     def set_value(self, index, value):
         temp = self.get(index)
         if temp:
             temp.value = value
             return True
         return False
+    # inserts a node based on provided index
     def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
         new_node = Node(value)
-        temp = self.get(index)
-        pre = self.get(index)
-        if temp:
-            new_node.next = self.head
-            self.head = new_node
-            return True
-        return False
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    
+    def remove_node(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index - 1)
+        # temp = self.get(index) // this is no good because it is O(n)
+        temp = prev.next # this method works better because it is O(1)
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            # sets temp backwards one node
+            temp.next = before
+            # before needs to be set prior to temp, so that it doesn't break the link between temp and after
+            before = temp
+            temp = after
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        print(fast.head.value)
+        return slow   
+    def has_loop(self):
+        slow  = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+    def remove_duplicates(self):
+        unique = set()
+        current = self.head
+        prev = None
+        while current:
+            if current.value in unique:
+                prev.next = current.next
+                self.length -= 1 
+            else:
+                unique.add(current.value)
+                prev = current
+            current = current.next
+    def binary_to_decimal(self):
+        current = self.head
+        decimalNum = 0
+        
+        while current:
+                decimalNum = decimalNum * 2 + current.value
+                print(decimalNum)
+                current = current.next
+        return decimalNum 
+    
+def find_kth_from_end(LinkedList, k):
+    slow = my_linked_list.head
+    fast = my_linked_list.head
+    if LinkedList.head == None:
+        return None
+    for _ in range(k):
+        if fast is None:
+            return None
+        fast = fast.next
+    while fast:
+        slow = slow.next
+        fast = fast.next
+    return slow
+
+            
 
 # test statements
 my_linked_list = LinkedList(4)
 my_linked_list.append(2)
 my_linked_list.append(3)
+my_linked_list.append(3)
+my_linked_list.append(3)
 my_linked_list.print_list()
-my_linked_list.set_value(1, 55)
-my_linked_list.print_list()
-my_linked_list.insert(1, 68)
+my_linked_list.remove_duplicates()
 my_linked_list.print_list()
